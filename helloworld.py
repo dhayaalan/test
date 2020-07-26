0cfc7fee8fcd539c3ba535e8b7f860b80b32f1bd
dhayaalan <68803746+dhayaalan@users.noreply.github.com>
2020-07-26 12:43:30 +0530

    Update helloworld.py
    test commit

--- helloworld.py
+++ helloworld.py
@@ -3,5 +3,7 @@ if len(name) < 3 :
     print("Name must be atleast 3 character")
 elif len(name) > 50 :
     print("Name can be max of 50 character")
+elif  len(name) > 50 & len(name) > 100 :
+    print("Name can be max of 50 character")
 else:
     print("Name looks good")
